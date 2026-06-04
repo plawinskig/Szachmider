b17e187ebd592c7490b515bb2ee6caec99cf1aa7
Grzegorz <mcgregor1406@gmail.com>
2026-06-04 18:21:34 +0200

    Ref board constructor

--- source/board/board.py
+++ source/board/board.py
@@ -5,7 +5,7 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[Square() for _ in range(width)] for _ in range(height)]
+        self.reset_board()
         
     def get_square(self, x, y):
         if 0 <= x < self.width and 0 <= y < self.height:
