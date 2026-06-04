3c63869c9cfc3492c99ab332fb3b55786405e226
Grzegorz <mcgregor1406@gmail.com>
2026-06-04 18:25:45 +0200

    Implement Square class

--- source/board/square.py
+++ source/board/square.py
@@ -1,5 +1,12 @@
 
 
 class Square:
-    pass
+    def __init__(self, piece=None):
+        self.piece = piece
+        
+    def is_empty(self):
+        return self.piece is None
+    
+    def __str__(self):
+        return str(self.piece) if self.piece else "."
 
