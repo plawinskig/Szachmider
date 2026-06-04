eac07da2493bff9fac4c235ff10a4ff4d46edd3e
Grzegorz <mcgregor1406@gmail.com>
2026-06-04 18:20:41 +0200

    Add basic board methods and constructor

--- source/board/board.py
+++ source/board/board.py
@@ -1,5 +1,28 @@
+from square import Square
 
 
 class Board:
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[Square() for _ in range(width)] for _ in range(height)]
+        
+    def get_square(self, x, y):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.board[y][x]
+        else:
+            raise IndexError("Square coordinates out of bounds")
+    
+    def move_piece(self, from_x, from_y, to_x, to_y):
+        if (0 <= from_x < self.width and 
+            0 <= from_y < self.height and 
+            0 <= to_x < self.width and 
+            0 <= to_y < self.height
+            ):
+            piece = self.board[from_y][from_x]
+            self.board[to_y][to_x] = piece
+            self.board[from_y][from_x] = Square()
+        else:
+            raise IndexError("Square coordinates out of bounds")
+        
 
