2d83c6041fb3cff85c09ad1dd4169a52d37c4a99
Grzegorz <mcgregor1406@gmail.com>
2026-06-06 20:50:02 +0200

    rearrange order of methods in board

--- source/board/board.py
+++ source/board/board.py
@@ -34,6 +34,16 @@ class Board:
         else:
             raise IndexError("Square coordinates out of bounds")
     
+    def is_empty_square(self, x: int, y: int) -> bool:
+        square = self.get_square(x, y)
+        return square.is_empty() if square else False
+    
+    def is_valid_position(self, x: int, y: int) -> bool:
+        return 0 <= x < self.width and 0 <= y < self.height
+    
+    def is_valid_move(self, from_x: int, from_y: int, to_x: int, to_y: int) -> bool:
+        return True # TODO
+    
     def move_piece(self, from_x: int, from_y: int, to_x: int, to_y: int):
         piece = self.get_piece(from_x, from_y)
         
@@ -45,16 +55,6 @@ class Board:
         
         self.set_piece(to_x, to_y, piece)
         self.set_square(from_x, from_y, Square())
-    
-    def is_empty_square(self, x: int, y: int) -> bool:
-        square = self.get_square(x, y)
-        return square.is_empty() if square else False
-    
-    def is_valid_position(self, x: int, y: int) -> bool:
-        return 0 <= x < self.width and 0 <= y < self.height
-    
-    def is_valid_move(self, from_x: int, from_y: int, to_x: int, to_y: int) -> bool:
-        return True # TODO
     
     def display(self):
         for row in self.board:
