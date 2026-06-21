63e1b50a68a6998b22842d09382b53217f600cdb
gplawinski <mcgregor1406@gmail.com>
2026-06-21 20:12:32 +0200

    add base bot class with abstract method for best move

---
+++ source/bot/base_bot.py
@@ -0,0 +1,23 @@
+
+from abc import ABC, abstractmethod
+from typing import Any, Callable
+
+from source.board.board import Board
+from source.board.piece import Piece
+
+Move = tuple[tuple[int, int], Callable[[], None], bool]
+BotMove = tuple[Piece, Move]
+
+
+class BaseBot(ABC):
+    def __init__(self, is_black: bool) -> None:
+        self.is_black = is_black
+
+    @abstractmethod
+    def get_best_move(
+        self,
+        board: Board,
+        move_history: Any | None = None,
+        time_limit: float | None = None,
+    ) -> BotMove | None:
+        raise NotImplementedError
